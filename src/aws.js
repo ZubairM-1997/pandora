97b796df453ae227a33994d65626eef4e1bf296d
Zubair <zubair.maqsood6@gmail.com>
2024-02-28 20:51:51 +0000

    starting the company sign up route

---
+++ src/aws.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const AWS = require('aws-sdk')
+
+AWS.config.update({
+	accessKeyId: process.env.AWS_ACCESS_KEY,
+	secretAccessKey: process.env.AWS_SECRET_KEY,
+	region: process.env.AWS_REGION
+});
+
+const dynamoDB = new AWS.DynamoDB();
+const s3Client = new AWS.S3();
+const cognitoServiceProvider = new AWS.CognitoIdentityServiceProvider();
+
+module.exports = {
+	dynamoDB,
+	s3Client,
+	cognitoServiceProvider
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,50 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const {
+	cognitoServiceProvider,
+	dynamoDB,
+	s3Client
+} = require('./aws.js')
 
-// Define routes
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON request body
+app.use(bodyParser.json());
+
+// Endpoint for company sign-up
 app.post('/api/company/sign_up', (req, res) => {
-    // Handle POST /api/company/sign_up
-    // You can access request body using req.body
-    console.log('Received POST request at /api/company/sign_up');
-    res.status(200).json({ message: 'Company signed up successfully!' });
+    const { username, email, password } = req.body;
+
+	const params = {
+		DesiredDeliveryMediums: [
+		   "SMS"
+		],
+		MessageAction: "SUPPRESS",
+		UserAttributes: [
+		{
+		  Name: "email",
+		  Value: email
+		 }
+		],
+		UserPoolId: process.env.AWS_USER_POOL_ID_COMPANIES,
+		Username: username
+	   }
+
+	cognitoServiceProvider.adminCreateUser(params, (err, data) => {
+		if (err) {
+            console.error('Error signing up:', err);
+            return res.status(500).json({ error: 'Failed to sign up user' });
+        }
+        // User signed up successfully
+        const cognitoUser = data.user;
+        res.status(200).json({ message: 'User signed up successfully' });
+	})
 });
 
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
